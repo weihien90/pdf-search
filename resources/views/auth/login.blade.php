9df9c6b7307ad3ba8cf0f91a9ca56b9697d56d75
weihien90 <weihien90@gmail.com>
2017-08-16 18:54:21 +0800

    Update login form view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
+            <div class="box box-info">
+                <div class="box-header">
+                    <h3 class="box-title">Login</h3>
+                </div>
+                <div class="box-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
